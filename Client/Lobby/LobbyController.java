e13227e9048f81b84449c611e648841af9e006ed
weberluk <luk_weber@hispeed.ch>
2017-07-19 11:10:37 +0200

    Lobby

--- Client/Lobby/LobbyController.java
+++ Client/Lobby/LobbyController.java
@@ -1,12 +1,18 @@
 package Lobby;
 
+import Client.ClientThreadForServer;
 import WindChuckers_Main.GameMenu_Model;
 import WindChuckers_Main.WindChuckers;
 import abstractClasses.Controller;
+import commonClasses.ServiceLocator;
+import javafx.application.Platform;
+import javafx.scene.input.KeyCode;
 
 public class LobbyController extends Controller<GameMenu_Model, LobbyView> {
 	private GameMenu_Model model;
 	private LobbyView view;
+	
+	ClientThreadForServer clientServer = ClientThreadForServer.getClientServer();
 	
 	public LobbyController(GameMenu_Model model, LobbyView view){
 		super(model,view);
@@ -17,6 +23,30 @@ public class LobbyController extends Controller<GameMenu_Model, LobbyView> {
 		 */
 		view.menuFileExitLobby.setOnAction(e -> {
 			view.stop();
+		});
+		
+		// Send-Button send Message to Server
+		view.btnSend.setOnAction(e -> {
+			String input = model.getUserNameString();
+			input += ": " + view.txtChat.getText();
+			model.messageContructorForChat(input);
+			view.txtChat.clear();
+		});
+		
+		// Enter for send the chat message
+		view.txtChat.setOnKeyPressed(e -> {
+			if (e.getCode().equals(KeyCode.ENTER)) {
+				String input = model.getUserNameString();
+				input += ": " + view.txtChat.getText();
+				model.messageContructorForChat(input);
+				view.txtChat.clear();
+			}
+		});
+		
+		// Watch the client for ChatMessage
+		clientServer.getChatMessageProperty().addListener((obervable, oldValue, newValue) -> {
+			ServiceLocator.getServiceLocator().getLogger().info("Message from Chat is arrived");
+			view.txtMessages.appendText("\n" + clientServer.getChatMessageInString());
 		});
 		
 	}
--- Client/Lobby/LobbyView.java
+++ Client/Lobby/LobbyView.java
@@ -3,6 +3,7 @@ package Lobby;
 import java.util.logging.Logger;
 
 import WindChuckers_Main.GameMenu_Model;
+import WindChuckers_Main.WindChuckers;
 import abstractClasses.View;
 import commonClasses.ServiceLocator;
 import commonClasses.Translator;
@@ -11,24 +12,41 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TitledPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class LobbyView extends View<GameMenu_Model> {
+	private static LobbyView lobbyView;
+	
 	private GameMenu_Model model;
     private Stage stage;
     
 	private Menu menuFileLobby;
 	protected MenuItem menuFileExitLobby;
 	
+	protected TitledPane chatPanel;
+	protected VBox chatBox;
+	protected Button btnSend;
+    public TextArea txtMessages;
+    protected TextField txtChat;
+    
+    protected Button btnGo;
+	
 
 	
 	public LobbyView (Stage stage, GameMenu_Model model){
 		super(stage,model);
-		
 		stage.setTitle("Lobby");
+		
+		lobbyView = this;
 
 	}
 
@@ -43,14 +61,39 @@ public class LobbyView extends View<GameMenu_Model> {
 		menuFileExitLobby = new MenuItem();
 		menuBar.getMenus().add(menuFileLobby);
 		
+		chatPanel = new TitledPane();
+		chatPanel.setExpanded(true);
+		chatPanel.setText("Chat");
+		chatPanel.setContent(chatBox);
+		
+		chatBox = new VBox();
+		btnSend = new Button();
+		txtMessages = new TextArea();
+		txtMessages.setEditable(false);
+		txtChat = new TextField();
+		btnGo = new Button();
+		Region region = new Region();
+		HBox btnBox = new HBox();
+		btnBox.setHgrow(region, Priority.ALWAYS);
+		btnBox.getChildren().addAll(btnSend, region, btnGo);
+		chatBox.getChildren().addAll(txtMessages,txtChat,btnBox);
+		
+		chatPanel = new TitledPane();
+		chatPanel.setExpanded(true);
+		chatPanel.setText("Chat");
+		chatPanel.setContent(chatBox);
+		
 		menuFileLobby.getItems().add(menuFileExitLobby);
 		
 		GridPane root = new GridPane();
 		root.add(menuBar, 0, 0);
+		root.add(chatPanel, 0, 1);
 		
 		updateTexts();
 		
         Scene scene = new Scene(root);
+        scene.getStylesheets().add(
+                getClass().getResource("Style.css").toExternalForm());
 		return scene;
 	}
 	
@@ -61,6 +104,14 @@ public class LobbyView extends View<GameMenu_Model> {
 		menuFileLobby.setText(t.getString("lobby.menu.file"));
 		menuFileExitLobby.setText(t.getString("lobby.menu.file.exit"));
 		
+		//Button
+		btnSend.setText(t.getString("program.btn.btnSend"));
+		btnGo.setText(t.getString("btnGo"));
+		
+	}
+	
+	public static LobbyView getLobbyView() {
+		return lobbyView;
 	}
 
 }
