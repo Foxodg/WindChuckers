ad796c5d991418888960128a21864d43d2fdb692
weberluk <luk_weber@hispeed.ch>
2017-07-25 07:13:14 +0200

    Only some comments

--- Client/MainMenu/MainMenuController.java
+++ Client/MainMenu/MainMenuController.java
@@ -25,9 +25,12 @@ public class MainMenuController extends Controller<GameMenu_Model, MainMenuView>
 		
 		
 		view.btnSingleGame.setOnAction(e -> {
+			//set option Sinlge-Game
 			model.setGameMode(1);
 			main.startApp();
+			//close the MainMenu_View
 			view.getStage().hide();
+			//set the SimpleBooleanProperty to start without more options
 			model.setDisableSingeleGame(true);
 			ServiceLocator.getServiceLocator().getLogger().info("SingleGame Disables all edits");
 			model.setDisableSingeleGame(false);
@@ -35,27 +38,37 @@ public class MainMenuController extends Controller<GameMenu_Model, MainMenuView>
 		
 		
 		view.btnStandardGame.setOnAction(e -> {
+			//set option Standard-Game
 			model.setGameMode(3);
 			main.startApp();
+			//close the MainMenu_View
 			view.getStage().hide();
+			//set the SimpleBooleanProperty to start without more options
 			model.setDisableSingeleGame(true);
 			ServiceLocator.getServiceLocator().getLogger().info("StandardGame Disables all edits");
 			model.setDisableSingeleGame(false);
 		});
 		
 		view.btnLongGame.setOnAction(e -> {
+			//set option Long-Game
 			model.setGameMode(7);
 			main.startApp();
+			//close the MainMenu_View
 			view.getStage().hide();
+			//set the SimpleBooleanProperty to start without more options
 			model.setDisableSingeleGame(true);
 			ServiceLocator.getServiceLocator().getLogger().info("LongGame Disables all edits");
 			model.setDisableSingeleGame(false);
 		});
 		
 		view.btnMarathonGame.setOnAction(e -> {
+			//set option Marathon-Game
 			model.setGameMode(15);
+			//start the main-App
 			main.startApp();
+			//close the MainMenu_View
 			view.getStage().hide();
+			//set the SimpleBooleanProperty to start without more options
 			model.setDisableSingeleGame(true);
 			ServiceLocator.getServiceLocator().getLogger().info("MarathonGame Disables all edits");
 			model.setDisableSingeleGame(false);
@@ -66,8 +79,10 @@ public class MainMenuController extends Controller<GameMenu_Model, MainMenuView>
 			windChuckers = WindChuckers.getWindChuckers();
 			windChuckers.startFriends();
 			view.getStage().hide();
+			//start the main-App only in the Background
 			main.startApp();
 			main.getGameMenuView().getStage().close();
+			//set the SimpleBooleanProperty to start the game with options
 			model.setEnableFriendsGame(true);
 			ServiceLocator.getServiceLocator().getLogger().info("Friends start for a Friend-Game set edits in game");
 			model.setEnableFriendsGame(false);
